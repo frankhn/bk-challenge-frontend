0f3ee0d30aed86cfb9d382c8f57d9e3a2240e5bc
frankhn <harfrank2@gmail.com>
2020-10-13 13:25:27 +0200

    adds a loader

---
+++ src/components/common/Spinner/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loader.css';
+
+export interface SpinnerProps {
+
+}
+
+const loader = () => (
+    <>
+        <div className="loadingio-spinner-eclipse-m1hvfar9ut"><div className="ldio-unu27bm2crj">
+            <div></div>
+        </div></div>
+    </>
+);
+
+export default loader;
