1857ecdaa797076f9308e827170b5a372e60fb38
frankhn <harfrank2@gmail.com>
2020-10-13 13:25:02 +0200

    updates component to hooks

--- src/components/applications/index.tsx
+++ src/components/applications/index.tsx
@@ -1,10 +1,10 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppState } from '../../store'
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
 import { getApplications } from './store/action'
-import { Link } from 'react-router-dom'
-import Modal from '../common/modals/Modal'
 import ViewApplicant from '../common/table/applicant'
+import updateObject from '../../helpers/updateObject'
+import './index.css'
 
 interface JobProps {
     getApplications(): any
@@ -16,55 +16,61 @@ interface State {
     application: any
 }
 
-class Applications extends Component<JobProps, State> {
+const initialState = {
+    modal: false,
+    application: {}
+}
 
-    state: State = {
-        modal: false,
-        application: {}
+const Applications = ({ applications }: JobProps) => {
+
+    const [state, setState] = useState(initialState)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getApplications())
+    }, [])
+
+    const modalHandler = () => {
+        return setState(updateObject(state, {
+            modal: !state.modal
+        }))
     }
 
-    componentDidMount() {
-        this.props.getApplications()
+    const applicationHandler = (application: any) => {
+        return setState(updateObject(state, {
+            modal: true, application
+        }))
     }
 
-    modalHandler = () => {
-        return this.setState(prevState => ({ modal: !prevState.modal }))
-    }
-
-    applicationHandler = (application: any) => {
-        return this.setState({ modal: true, application })
-    }
-
-
-    render() {
-        return (
-            <>
-                {this.props.applications && this.props.applications
-                    ? this.props.applications.map(application => (
-                        <div className="job" onClick={() => this.applicationHandler(application)} style={{ cursor: "pointer"}}>
-                            <div >
-                                <h3>
-                                    <strong>
-                                        {`${application.User.firstname} ${application.User.lastname}`}
-                                    </strong>
-                                </h3>
-                                <h5>Job Applied</h5>
-                                <p> Job {application.Job.title}</p>
-                                <p> Descr{application.Job.description}</p>
-                            </div>
+    return (
+        <div className="col-md-6" style={{ padding: "0 5rem" }}>
+            <div style={{ marginBottom: "2.8rem",textAlign: "center",fontSize: "1.5rem", fontWeight: 800 }}>Applications</div>
+            {applications && applications
+                ? applications.map(application => (
+                    <div className="application" onClick={() => applicationHandler(application)} style={{ cursor: "pointer" }}>
+                        <div >
+                            <h3 className="appli-name">
+                                <strong>
+                                    {`${application.User.firstname} ${application.User.lastname}`}
+                                </strong>
+                            </h3>
+                            <h6 style={{ color: "#ccc"}}>Job Applied</h6>
+                            <p> Job {application.Job.title}</p>
+                            <p> Descr{application.Job.description}</p>
                         </div>
-                    ))
-                    : <div>fetching available applications... </div>}
+                    </div>
+                ))
+                : <div>fetching available applications... </div>}
 
 
-                <ViewApplicant
-                    application={this.state.application}
-                    show={this.state.modal}
-                    togglemodal={this.modalHandler}
-                />
-            </>
-        )
-    }
+            <ViewApplicant
+                application={state.application}
+                show={state.modal}
+                togglemodal={modalHandler}
+            />
+        </div>
+    )
 }
 
 const mapStateToProps = (state: AppState) => {
